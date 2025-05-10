46d4cb3f43271f271eacf7dcabddebdf36e89c0e
Isaiasiv <isaias.ribeiro@estudante.ifgoiano.edu.br>
2025-05-10 00:10:59 -0300

    inicio

---
+++ src/main/java/main/servidor_grpc/ServidorGRpcApplication.java
@@ -0,0 +1,13 @@
+package main.servidor_grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServidorGRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/servidor_grpc/ServidorGRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package main.servidor_grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorGRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
